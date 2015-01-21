7d04550c2a0c091ec728b7f09ab0c607502a8d92
Kdy <im@kdy.ch>
2015-01-21 17:11:42 +0100

    *heavy breathing*
    Ident, please!

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
-/*
-TODO: 	Timezone du client,
-		ajouter le random de gifs selon les états,
-		ajouter les images tout-court
-		c'pas beau, rendre le truc plus joli
-*/
+	/*
+	TODO: 	Timezone du client
+		Ajouter le random de gifs selon les états
+		Ajouter les images tout court
+	*/
 
-$day = date("N");
-$time = date("H:i");
+	$day = date("N");
+	$time = date("H:i");
 
-if($day == 1) {
-	$message = "#LigueContreLeLundi";
-} elseif($day == 5) {
-	$message = "Vendredi ? LOLNOPE.";
-} elseif($time >= "11:45" && $time < "13:00") {
-	$message = "Ah bah non, 'faut aller manger maintenant !";
-} elseif($time >= "17:30" && $time < "23:00") {
-	$message = "Boarf, trop tard maintenant...";
-} elseif($time >= "23:00" && $time < "05:00") {
-	$message = "Et pourquoi pas aller dormir, vu l'heure ?";
-} else {
-	$message = "Hmm, ça semble safe...";
-}
+	if($day == 1) {
+		$message = "#LigueContreLeLundi";
+	} elseif($day == 5) {
+		$message = "Vendredi ? LOLNOPE.";
+	} elseif($time >= "11:45" && $time < "13:00") {
+		$message = "Ah bah non, 'faut aller manger maintenant !";
+	} elseif($time >= "17:30" && $time < "23:00") {
+		$message = "Boarf, trop tard maintenant...";
+	} elseif($time >= "23:00" && $time < "05:00") {
+		$message = "Et pourquoi pas aller dormir, vu l'heure ?";
+	} else {
+		$message = "Hmm, ça semble safe...";
+	}
+	
 	/**
 	 * Rendu HTML
 	 */
