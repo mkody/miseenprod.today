8b4141ac13320924584b1530e545bbc3e4029539
Gregoire Daussin <gregoire.daussin@caudalie.com>
2015-01-21 17:31:41 +0100

    Afficher les gifs

--- index.php
+++ index.php
@@ -1,25 +1,57 @@
 <?php
 	/*
-	TODO: 	Timezone du client
-		Ajouter le random de gifs selon les états
-		Ajouter les images tout court
+	TODO
+	- Timezone du client
+	- Icone de github
+	- Random message ?
+	- Récupérer la date/heure du client
 	*/
 
 	$day = date("N");
 	$time = date("H:i");
+	$message = "Hmm, ça semble safe...";
+	$directory = "prod";
 
+	/**
+	 * Mise en prod ou pas alors ?
+	 */
 	if($day == 1) {
 		$message = "#LigueContreLeLundi";
+		$directory = "lundi";
 	} elseif($day == 5) {
 		$message = "Vendredi ? LOLNOPE.";
+		$directory = "vendredi";
 	} elseif($time >= "11:45" && $time < "13:00") {
 		$message = "Ah bah non, 'faut aller manger maintenant !";
+		$directory = "midi";
 	} elseif($time >= "17:30" && $time < "23:00") {
 		$message = "Boarf, trop tard maintenant...";
+		$directory = "soir";
 	} elseif($time >= "23:00" && $time < "05:00") {
 		$message = "Et pourquoi pas aller dormir, vu l'heure ?";
-	} else {
-		$message = "Hmm, ça semble safe...";
+		$directory = "dormir";
+	}
+
+	/**
+	 * Allons chercher un gif pour fêter ça
+	 */
+	$file = "";
+	try {
+		$scan = scandir("images/" . $directory);
+		if ($scan === FALSE) throw new Exception();
+
+		$files = array();
+		foreach($scan as $item) {
+			if ($item !== NULL && $item !== "" &&
+				strpos($item, ".gif", strlen($item) - 4) !== FALSE) {
+				$files[] = $item;
+			}
+		}
+
+		$rand = rand(0, count($files) - 1);
+		$file = $files[$rand];
+	} catch (Exception $e) {
+
 	}
 	
 	/**
@@ -29,6 +61,6 @@
 	$html_headline = "Mise en prod, today ?";
 	$html_message = $message;
 	$html_notice = "Amélioration du site dans quelques temps, mais pas avant mardi hein.";
-	$html_gif = "";
+	$html_gif = $file;
 
 	include_once("view.php");
